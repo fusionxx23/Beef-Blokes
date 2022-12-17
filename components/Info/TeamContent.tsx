ae13b914b71daaf9963daeb6824f9f12a41cd7e2
TarpCityBitch <travistarpley0@gmail.com>
2022-12-17 11:04:23 -0800

    Meet the team sizing

--- components/Info/TeamContent.tsx
+++ components/Info/TeamContent.tsx
@@ -7,8 +7,8 @@ export default function TeamContent() {
   const transform = 'rotate(5.62deg)'
   return (
     <div className="flex flex-col items-center w-full font-sniglet text-white h-full" style={{ marginTop: '34px' }}>
-      <h2 className='text-2xl hidden sm:block' style={{ transform, fontSize: '45px' }}>meet the</h2>
-      <h1 className='font-extrabold hidden sm:block' style={{ transform, fontSize: '190px', lineHeight:'190px' }}>TEAM</h1>
+      <h2 className='text-2xl hidden sm:block sm:text-4xl' style={{ transform, }}>meet the</h2>
+      <h1 className='font-extrabold hidden sm:block sm:text-8xl md:text-9xl  lg:text-11xl leading-8' style={{ transform }}>TEAM</h1>
       
       <h2 className='text-2xl sm:hidden' style={{ transform, fontSize: '35px' }}>meet the</h2>
       <h1 className='font-extrabold sm:hidden' style={{ transform, fontSize: '100px', lineHeight:'100px' }}>TEAM</h1>
@@ -33,7 +33,7 @@ export default function TeamContent() {
 
 function MemberCard({src, name, title, desc}: {src: StaticImageData, name: string, title: string, desc:string}) {
   return (
-    <div className='md:mx-4 lg:mx-10 text-center text-black'>
+    <div className='md:mx-3 lg:mx-10 text-center text-black'>
       <Image className='hidden sm:block sm:w-40 md:w-56 lg:w-64 xl:w-80 2xl:w-96' src={src} alt={'member card'}></Image>
       <Image className='sm:hidden' src={src} style={{width:'400px'}} alt={'member card'}></Image>
       <h1 style={{fontSize:'32px'}}>{name}</h1>
